bb9955780eedbfdad2bb3a58d6fde99c9e32f4d9
Gareth-Moore <gccsmoore3@gmail.com>
2023-02-13 14:04:43 +0000

    upload

--- Calendar/cal_obj.js
+++ Calendar/cal_obj.js
@@ -1,69 +1,98 @@
 const calendar_months = [
-    jan: {
-        name: "January",
-        position: 0,
-        days: 31
-    }
-    feb: {
-        name: "Febuary",
-        position: 1,
-        days: 28
-    }
-    mar: {
-        name: "March",
-        position: 2,
-        days: 31
-    }
-    apr: {
-        name: "April",
-        position: 3,
-        days: 30
-    }
-    may: {
-        name: "May",
-        position: 4,
-        days: 31
-    }
-    jun: {
-        name: "june",
-        position: 5,
-        days: 30
-    }
-    jul: {
-        name: "July",
-        position: 6,
-        days: 31
-    }
-    aug: {
-        name: "August",
-        position: 7,
-        days: 31
-    }
-    sep: {
-        name: "September",
-        position: 8,
-        days: 30
-    }
-    oct: {
-        name: "October",
-        position:9,
-        days: 31
-    }
-    nov: {
-        name: "November",
-        position: 10,
-        days: 30
-    }
-    dec: {
-        name: "December",
-        position: 11,
-        days: 31
-    }
+    { 
+        jan: {
+            name: "January",
+            position: 0,
+            total_days: 31,
+            start_day: 6,
+        }
+    },
+    { 
+        feb: {
+            name: "Febuary",
+            position: 1,
+            total_days: 28,
+            start_day: 2,
+        }
+    },
+    { 
+        mar: {
+            name: "March",
+            position: 2,
+            total_days: 31,
+            start_day: 2,
+        }
+    },
+    { 
+        apr: {
+            name: "April",
+            position: 3,
+            total_days: 30,
+            start_day: 5,
+        }
+    },
+    { 
+        may: {
+            name: "May",
+            position: 4,
+            total_days: 31,
+            start_day: 0,
+        }
+    },
+    { 
+        jun: {
+            name: "June",
+            position: 5,
+            total_days: 30,
+            start_day: 3,
+        }
+    },
+    { 
+        jul: {
+            name: "July",
+            position: 6,
+            total_days: 31,
+            start_day: 5,
+        }
+    },
+    { 
+        aug: {
+            name: "August",
+            position: 7,
+            total_days: 31,
+            start_day: 1,
+        }
+    },
+    { 
+        sep: {
+            name: "September",
+            position: 8,
+            total_days: 30,
+            start_day: 4,
+        }
+    },
+    { 
+        oct: {
+            name: "October",
+            position: 9,
+            total_days: 31,
+            start_day: 6,
+        }
+    },
+    { 
+        nov: {
+            name: "November",
+            position: 10,
+            total_days: 30,
+            start_day: 2,
+        }
+    },
+    { 
+        decc: {
+            name: "December",
+            position: 11,
+            total_days: 31,
+            start_day: 4,
+        }
+    },
 ]
-
-
-
-
-// 30 - sep april june nov
-// 31 - re
-// 28 - feb
