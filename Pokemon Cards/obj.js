45dd1f0a9296b9653746e509d2471fb861d45941
Gareth-Moore <gccsmoore3@gmail.com>
2023-04-13 22:39:04 +0100

    changed search bar positioning

--- Pokemon Cards/obj.js
+++ Pokemon Cards/obj.js
@@ -1,18 +1,18 @@
 const pokemonList = []; // create an empty array to store the Pokemon objects
 
-fetch('https://pokeapi.co/api/v2/pokemon/?limit=151')
-  .then(response => response.json())
+fetch("https://pokeapi.co/api/v2/pokemon/?limit=151")
+  .then((response) => response.json())
   .then(function (allpokemon) {
     allpokemon.results.forEach(function (pokemon) {
       fetchGenOneData(pokemon);
-    })
+    });
   });
 
 function fetchGenOneData(pokemon) {
   let url = pokemon.url;
 
   fetch(url)
-    .then(response => response.json())
+    .then((response) => response.json())
     .then(function (pokeData) {
       let pokemonObj = {}; // create an empty object to store the Pokemon data
 
@@ -20,11 +20,15 @@ function fetchGenOneData(pokemon) {
       pokemonObj.name = pokeData.name;
       pokemonObj.sprite = pokeData.sprites.front_default;
       pokemonObj.id = pokeData.id;
-      pokemonObj.moves = [{ name: pokeData.moves[0].move.name, description: "" }
+      pokemonObj.moves = [
+        { name: pokeData.moves[0].move.name, description: "" },
       ];
 
       if (pokeData.moves.length > 1) {
-        pokemonObj.moves.push({ name: pokeData.moves[1].move.name, description: "" });
+        pokemonObj.moves.push({
+          name: pokeData.moves[1].move.name,
+          description: "",
+        });
       }
 
       pokemonObj.stats = {
@@ -33,13 +37,14 @@ function fetchGenOneData(pokemon) {
         def: pokeData.stats[2].base_stat,
         sp_atk: pokeData.stats[3].base_stat,
         sp_def: pokeData.stats[4].base_stat,
-        spd: pokeData.stats[5].base_stat
+        spd: pokeData.stats[5].base_stat,
       };
 
       // call the getMoveInfo functions to fetch the move descriptions and add them to the Pokemon object
       getMoveInfo(pokeData.moves[0].move.url)
-        .then(moveData => {
-          pokemonObj.moves[0].description = moveData.flavor_text_entries[4].flavor_text;
+        .then((moveData) => {
+          pokemonObj.moves[0].description =
+            moveData.flavor_text_entries[4].flavor_text;
 
           if (pokeData.moves.length > 1) {
             return getMoveInfo(pokeData.moves[1].move.url);
@@ -47,34 +52,32 @@ function fetchGenOneData(pokemon) {
             return Promise.resolve();
           }
         })
-        .then(moveData => {
+        .then((moveData) => {
           if (moveData) {
-            pokemonObj.moves[1].description = moveData.flavor_text_entries[4].flavor_text;
+            pokemonObj.moves[1].description =
+              moveData.flavor_text_entries[4].flavor_text;
           }
 
           pokemonList.push(pokemonObj); // add the Pokemon object to the list
-         // console.log(pokemonObj); // log the Pokemon object to the console (optional)
+          // console.log(pokemonObj); // log the Pokemon object to the console (optional)
         });
     });
 }
 
 function getMoveInfo(moveUrl) {
   return fetch(moveUrl)
-    .then(response => response.json())
-    .then(moveData => {
+    .then((response) => response.json())
+    .then((moveData) => {
       return moveData;
     });
 }
 // all of the above creates the pokemon objects
 // the below searches through the object
 
-
-
-
-const form = document.querySelector('form');
+const form = document.querySelector("form");
 const input = document.querySelector('input[type="text"]');
 
-form.addEventListener('submit', function(e) {
+form.addEventListener("submit", function (e) {
   e.preventDefault(); // prevent the form from submitting
 
   const searchQuery = input.value.toLowerCase(); // get the search query entered by the user
@@ -92,11 +95,6 @@ form.addEventListener('submit', function(e) {
   }
 
   if (!foundMatch) {
-    alert('Please enter a valid Pokemon name or ID.');
+    alert("Please enter a valid Pokemon name or ID.");
   }
 });
-
-
-
-  
-  
